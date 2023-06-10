9406f231e7ae6488c1c936769eb3f87fe0899844
momomuchu <mohamedmaache68@gmail.com>
2023-06-10 20:13:47 +0200

    c

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ const jokeBtn = document.getElementById('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
+// Create a new button element
+var newButton = document.createElement("button");
+newButton.innerHTML = "New Button";
+
+// Add an event listener to the new button
+newButton.addEventListener("click", function() {
+  // Perform some action when the new button is clicked
+  console.log("New button clicked!");
+});
+
+// Find the container element where you want to add the new button
+var container = document.querySelector(".container");
+
+// Append the new button to the container
+container.appendChild(newButton);
